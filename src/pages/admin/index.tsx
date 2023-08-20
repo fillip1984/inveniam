240fe0c1cb3bfc505bcb3dd9733b6ce26ec0f22b
fillip1984 <fillip1984@gmail.com>
2023-08-19 20:45:26 -0400

    modularized admin, also working on tag management

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,25 @@
+import { type Tag } from "@prisma/client";
 import { format } from "date-fns";
 import { useState, type ChangeEvent } from "react";
+import { BsPencil } from "react-icons/bs";
 import { api } from "~/utils/api";
 
 export default function AdminPage() {
+  return (
+    <div className="mx-auto flex w-3/4 flex-col gap-4 pt-2">
+      <h3>Admin</h3>
+
+      <ExportSection />
+
+      <ImportSection />
+
+      <TagSection />
+    </div>
+  );
+}
+
+const ExportSection = () => {
   const [isPrettyPrint, setIsPrettyPrint] = useState(true);
-  const [importFile, setImportFile] = useState<File | undefined>(undefined);
 
   const { mutate: exportData } = api.admin.export.useMutation({
     onSuccess: (data) => {
@@ -29,12 +44,39 @@ export default function AdminPage() {
     },
   });
 
-  const { mutate: importData } = api.admin.import.useMutation();
-
   const handleExport = () => {
     console.log("exporting");
     exportData();
   };
+
+  return (
+    <div className="flex flex-col items-start gap-1">
+      <h4>Export</h4>
+      <p>Export boards and a portion of the tasks (excludes attachments)</p>
+      <button
+        type="button"
+        onClick={handleExport}
+        className="rounded bg-primary px-4 py-2">
+        Export
+      </button>
+
+      <label className="relative inline-flex cursor-pointer items-center">
+        <input
+          type="checkbox"
+          checked={isPrettyPrint}
+          onChange={() => setIsPrettyPrint((prev) => !prev)}
+          className="peer sr-only"
+        />
+        <div className="peer h-6 w-11 rounded-full bg-primary/30 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:bg-white after:transition-all after:content-[''] peer-checked:bg-accent2 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4"></div>
+        <span className="ml-3">Pretty Print JSON</span>
+      </label>
+    </div>
+  );
+};
+
+const ImportSection = () => {
+  const [importFile, setImportFile] = useState<File | undefined>(undefined);
+  const { mutate: importData } = api.admin.import.useMutation();
 
   const handleImport = () => {
     console.log("importing");
@@ -62,46 +104,44 @@ export default function AdminPage() {
   };
 
   return (
-    <div className="mx-auto flex w-3/4 flex-col gap-4 pt-2">
-      <h3>Admin</h3>
+    <div className="flex flex-col items-start gap-1">
+      <h4>Import</h4>
+      <p>Import boards and a portion of the tasks (excludes attachments)</p>
+      <input
+        type="file"
+        accept="application/json"
+        onChange={handleFileChange}
+      />
+      <button
+        type="button"
+        onClick={handleImport}
+        className="rounded bg-primary px-4 py-2">
+        Import
+      </button>
+    </div>
+  );
+};
 
+const TagSection = () => {
+  const { data: tags } = api.tags.readAll.useQuery();
+
+  const handleEditTag = (tag: Tag) => {
+    console.log(`editing tag ${tag.name}`);
+  };
+
+  return (
+    <div>
+      <h4>Tags</h4>
       <div className="flex flex-col items-start gap-1">
-        <h4>Export</h4>
-        <p>Export boards and a portion of the tasks (excludes attachments)</p>
-        <button
-          type="button"
-          onClick={handleExport}
-          className="rounded bg-primary px-4 py-2">
-          Export
-        </button>
-
-        <label className="relative inline-flex cursor-pointer items-center">
-          <input
-            type="checkbox"
-            checked={isPrettyPrint}
-            onChange={() => setIsPrettyPrint((prev) => !prev)}
-            className="peer sr-only"
-          />
-          <div className="peer h-6 w-11 rounded-full bg-primary/30 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:bg-white after:transition-all after:content-[''] peer-checked:bg-accent2 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4"></div>
-          <span className="ml-3">Pretty Print JSON</span>
-        </label>
-      </div>
-
-      <div className="flex flex-col items-start gap-1">
-        <h4>Import</h4>
-        <p>Import boards and a portion of the tasks (excludes attachments)</p>
-        <input
-          type="file"
-          accept="application/json"
-          onChange={handleFileChange}
-        />
-        <button
-          type="button"
-          onClick={handleImport}
-          className="rounded bg-primary px-4 py-2">
-          Import
-        </button>
+        {tags?.map((tag) => (
+          <div key={tag.id} className="flex items-center gap-2">
+            <div className={`rounded bg-accent2 p-1`}>{tag.name}</div>
+            <button type="button" onClick={() => handleEditTag(tag)}>
+              <BsPencil />
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
